d9404252bc75691c320a0f2a246abb435c384356
tiffany <tjiang28@dtechhs.org>
2026-01-23 11:01:05 +0000

    added like half my buttons + title

--- src/Prematch.tsx
+++ src/Prematch.tsx
@@ -1,14 +1,43 @@
-
+import './Prematch.css'
 
 function Prematch() {
 
 
   return (
-    <>
+    <div>
+       <h2 className="PrematchTitle text-3xl text-white font-bold tracking-wide">
+          PREMATCH
+        </h2>
+      <div>
+        <textarea
+        className='textbox1'
+        placeholder='Match Number'
+        content="center"
+        />
+      </div>
+
+      <div>
+        <textarea
+        className='textbox2'
+        placeholder='Team Number'
+        content="center"
+        />
+      </div>
+      <div className='container1'>
+        <button className='orient'>
+          Orientation
+        </button>
+        <button className='alliance'>
+          Alliance
+        </button>
+
+      </div>
+
+
     
      
      
-    </>
+    </div>
   )
 }
 
