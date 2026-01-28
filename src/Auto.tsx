b6ab5bb4a38c7e8b2525fae9ad64fd6c27ae8de8
lmoriarty27-star <lmoriarty27@dtechhs.org>
2026-01-27 17:13:04 -0800

    auto

--- src/Auto.tsx
+++ src/Auto.tsx
@@ -1,14 +1,48 @@
-
+import './Auto.css'
 
 function Auto() {
 
 
     return (
-      <>
-      
-       
-       
-      </>
+      <div>
+        <div className='ContainerTitle'>
+          <h2 className='Auto text -3xl'>AUTO</h2>
+        </div>
+        <div className='ContainerTime'>
+          <div>
+            <button className='Pass'>
+            Pass
+          </button>
+         </div>
+
+         <div> 
+            <button className='Score'>
+            Score
+          </button>
+         </div>
+        </div>
+
+        <div className='ContainerTower'>
+          <div>
+            <button className='ClimbLeft'>
+            Climb Left
+            </button>
+          </div>
+
+          
+          <div>
+            <button className='ClimbMiddle'>
+            Climb Middle
+            </button>
+          </div>
+
+          <div>
+            <button className='ClimbRight'>
+            Climb Right
+            </button>
+          </div>
+        </div>
+      </div>
     )
   }
   
